47c26e01782b35d7200efe6762e1b38d058eb2d3
Afonso de Mori <me@afonso.dev>
2023-07-17 02:42:06 +0200

    feat: Add Will there be enough space? challenge

---
+++ src/dev/afonso/codewars/java/kyu8/c002/Bob.java
@@ -0,0 +1,16 @@
+package dev.afonso.codewars.java.kyu8.c002;
+
+/**
+ * Solves the challenge <a href="">Will there be enough space?</a>
+ */
+public class Bob {
+    /**
+     * @param cap is the amount of people the bus can hold excluding the driver.
+     * @param on is the number of people on the bus excluding the driver.
+     * @param wait is the number of people waiting to get on to the bus excluding the driver.
+     * @return If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+     */
+    public static int enough(int cap, int on, int wait) {
+        return (cap - on >= wait) ? 0 : on + wait - cap;
+    }
+}
